b0b5f96c5e0f181352ab67c46ed429a4f48a64cb
Lea Wilhelmer <leawilhelmer@gmail.com>
2020-10-23 09:30:45 -0700

    added questions and answers

--- app.js
+++ app.js
@@ -1,23 +1,100 @@
-var h1Message = document.querySelector("#message");
+var timerCount = 75
+var timerEl = document.querySelector("#timer")
+
+timerEl.textContent = timerCount
+
+var timer = window.setInterval(function () {
+  timerCount--;
+  timerEl.textContent = timerCount
+
+  if (timerCount === 0) {
+    clearInterval(timer)
+  }
+}, 1000)
+
+var questions = [
+  {
+    q: "Web API supports which of the following protocol?",
+    choices: ["TCP", "HTTP", "Soap"],
+    a: "B"
+  },
+  {
+    q: "What is a correct syntax to output "Hello World" in Java?",
+    choices: ["System.out.printIn("Hello World");", "print("Hello World");", "Console.WriteLine("Hello World");"],
+    a: "A"
+  },
+  {
+    q: "How do you insert COMMENTS in Java code?",
+    choices: ["/* This is a comment", "// This is a comment", "# This is a comment"],
+    a: "B"
+  },
+  {
+    q: "Which data type is used to create a variable that should store text?",
+    choices: ["myString", "string", "String"],
+    a: "C"
+  },
+  {
+    q: "How do you create a variable with the numeric value 5?",
+    choices: ["x = 5", "num x = 5", "int x = 5"],
+    a: "C"
+  },
+
+]
 
 
-var thing = function () {
-  console.log("thing")
+var questionsPointer = 0
+
+var question = document.querySelector("#question")
+var optionA = document.querySelector("#optionA")
+var optionB = document.querySelector("#optionB")
+var optionC = document.querySelector("#optionC")
+
+
+function setQuestions() {
+
+  if (questionsPointer === questions.length) {
+    clearInterval(timer)
+    alert("You are done with " + timerCount + " time left")
+    return
+  }
+
+  question.textContent = questions[questionsPointer].q
+  optionA.textContent = questions[questionsPointer].choices[0]
+  optionB.textContent = questions[questionsPointer].choices[1]
+  optionC.textContent = questions[questionsPointer].choices[2]
 }
 
+setQuestions()
 
+optionA.addEventListener("click", function () {
+  if (optionA.getAttribute("data-answer") === questions[questionsPointer].a) {
+    console.log("This is correct")
+    questionsPointer++
+    setQuestions()
+  } else {
+    console.log("wrong")
+    timerCount -= 10
+  }
+})
 
-var count = 10
-function setTime() {
-  var timer = setInterval(function () {
-    count--
-    console.log(count)
-    if (count === 0) {
-      console.log("STOOOOOPPED")
-      h1Message.textContent = "WOW THIS IS ROUUUUGH"
-      clearInterval(timer)
-    }
-  }, 1000)
-}
+optionB.addEventListener("click", function () {
+  if (optionB.getAttribute("data-answer") === questions[questionsPointer].a) {
+    console.log("This is correct")
+    questionsPointer++
+    setQuestions()
+  } else {
+    console.log("wrong")
+    timerCount -= 10
+  }
+})
 
-setTime()
+optionC.addEventListener("click", function () {
+  if (optionC.getAttribute("data-answer") === questions[questionsPointer].a) {
+    console.log("This is correct")
+    questionsPointer++
+    setQuestions()
+  } else {
+    console.log("wrong")
+    timerCount -= 10
+  }
+})
